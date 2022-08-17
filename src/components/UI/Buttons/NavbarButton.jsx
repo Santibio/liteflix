1306b53462d03fb0de7808261331bda5ae7710c4
Santibio <87582357+Santibio@users.noreply.github.com>
2022-08-17 20:40:28 -0300

    Fix(NavbarButton) - middle bar

--- src/components/UI/Buttons/NavbarButton.jsx
+++ src/components/UI/Buttons/NavbarButton.jsx
@@ -31,7 +31,6 @@ div {
 
     :nth-child(2) {
       opacity: ${({ open }) => open ? '0' : '1'};
-      transform: ${({ open }) => open ? 'translateX(20px)' : 'translateX(0)'};
     }
 
     :nth-child(3) {
